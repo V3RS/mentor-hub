800af3aa49ca101f1700dad7412f4c9a6704704a
Michael Stedman <michaelstedman81@gmail.com>
2020-12-19 20:00:31 -0700

    Removed boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-//import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
 import { Header }  from "./components/Header/"
 import SocialMedia from "./components/SocialMedia"
 
