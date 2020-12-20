b3c49dec7121a21e61f19b836e7be5c9bfd10bd4
Michael Stedman <michaelstedman81@gmail.com>
2020-12-19 20:07:15 -0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import { Header }  from "./components/Header/"
 import SocialMedia from "./components/SocialMedia"
 
+const AppContainer = styled.div`
+  border: 2px solid dodgerblue;
+`
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SocialMedia />
-    </div>
-  );
+    <AppContainer>
+      <p>App Comppne</p>
+    </AppContainer>
+  )
 }
 
 export default App;
